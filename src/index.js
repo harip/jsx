43182946b50188819fa496e5b031e40faafad9fa
Hari <charanp@gmail.com>
2020-11-06 00:01:58 -0500

    session 2 changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click Me';
+}
+
 // Create component
-const App = () => {
-  return <div>Hi there!</div>
+const App = () => { 
+  return  (
+    <div>
+      <label className="label" for="name">Enter Name:</label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+    </div>
+  );
 };
 
 // Take the component and show it on screen
